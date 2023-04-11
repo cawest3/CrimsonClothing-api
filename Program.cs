8f8eb65663ade59ded60182e37a4d450f9a2ed61
Jared Burnham <jaredburnham@Jareds-MacBook-Pro.local>
2023-04-10 20:38:23 -0500

    Controllers Fix

--- Program.cs
+++ Program.cs
@@ -1,13 +1,60 @@
 using Microsoft.AspNetCore.Cors;
+// var builder = WebApplication.CreateBuilder(args);
+
+// // Add services to the container.
+
+// builder.Services.AddControllers();
+// builder.Services.AddControllers();
+// {
+//     OptionsBuilderConfigurationExtensions.AddPolicy("OpenPolicy");
+// }
+// // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
+
+// var app = builder.Build();
+
+// // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+// app.UseHttpsRedirection();
+
+// app.UseAuthorization();
+
+// app.MapControllers();
+
+// app.Run();
+
+
+System.Console.WriteLine("Sup.");
+// DeleteSong.DropSongTable();
+// SaveSong.CreateSongTable();
+
+// ReadSongData theSongs = new ReadSongData();
+// theSongs.GetAllSongs();
+
+// Song mySong = new Song(){Title="Sanctuary", Artist="Joji"};
+// mySong.Save.CreateSong(mySong);
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddControllers();
+builder.Services.AddCors(options =>
 {
-    OptionsBuilderConfigurationExtensions.AddPolicy("OpenPolicy");
-}
+    options.AddPolicy("OpenPolicy", 
+    builder =>
+    {
+        builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -25,6 +72,9 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseCors("OpenPolicy");
+
 app.MapControllers();
 
 app.Run();
+
