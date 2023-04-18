e8a6a6051089480bb6ae0b2dbac2c3db0cb66e0d
Jared Burnham <jaredburnham@Jareds-MacBook-Pro.local>
2023-04-17 19:11:53 -0500

    Transaction Modelv2

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -3,7 +3,7 @@ namespace CrimsonClothing_api.Models
     public class Transaction
     {
         public int transactionId {get; set;}
-        public int profit {get; set;}
+        public decimal profit {get; set;}
         public int customerId {get; set;}
         public int itemId{get; set;}
 
