8815347d18f3f789a269381e97fd1944cb70d101
Jared Burnham <jaredburnham@Jareds-MacBook-Pro.local>
2023-04-19 18:44:06 -0500

    Admin Read

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -17,7 +17,9 @@ namespace CrimsonClothing_api.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            List<Admin> admins = new List<Admin>();
+            ReadAdmin readin = new ReadAdmin();
+            admins = readin.GetAdmins();
         }
 
         // GET: api/Admin/Get/5
